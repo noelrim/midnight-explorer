2429474d8d8b573157296adbc05b15d0a0e41839
Noel Rimbert <noel.rimbert@iohk.io>
2025-06-04 15:17:14 +0200

    added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopBar from './components/TopBar';
+import PasswordGate from './components/PasswordGate';
 import Dashboard from './pages/Dashboard';
 import TransactionDetails from './pages/TransactionDetails';
 import BlockDetails from './pages/BlockDetails';
@@ -8,16 +9,22 @@ import SPODetails from './pages/SPODetails';
 import SPOList from './pages/SPOList';
 import ChainActivity from './pages/ChainActivity';
 
-// import other pages...
 
 export default function App() {
+  const [unlocked, setUnlocked] = useState(false);
+
+  if (!unlocked) return <PasswordGate onAccessGranted={() => setUnlocked(true)} />;
+
+
+
+
   return (
     <Router>
       <TopBar />
-      <Routes> 
+      <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/spos" element={<SPOList />} />
-        <Route path="/transaction/:hash" element={<TransactionDetails />} /> 
+        <Route path="/transaction/:hash" element={<TransactionDetails />} />
         <Route path="/block/:height" element={<BlockDetails />} />
         <Route path="/spo/:author" element={<SPODetails />} />
         <Route path="/chainactivity" element={<ChainActivity />} />
